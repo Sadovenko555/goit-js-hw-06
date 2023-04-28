c2624c4b67d0c86ff1c44762a225efadb5218005
Ihor <sadovenko555@gmail.com>
2023-04-28 22:43:41 +0300

    add scripts

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Будь ласка, заповніть усі поля форми!');
+    return;
+  }
+
+  const formData = { email, password };
+  console.log(formData);
+
+  form.reset();
+});
