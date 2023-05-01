d3865a2c9d2698dcb426382690f2200c9f889c38
Ihor <sadovenko555@gmail.com>
2023-05-01 09:58:44 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,48 @@
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('#controls [data-create]');
+const destroyBtn = document.querySelector('#controls [data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+function createBox(size) {
+  const box = document.createElement('div');
+  box.style.width = `${size}px`;
+  box.style.height = `${size}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  return box;
+}
+
+
+function createBoxes(amount) {
+  let size = 30;
+  const boxes = [];
+  for (let i = 0; i < amount; i += 1) {
+    const box = createBox(size);
+    boxes.push(box);
+    size += 10;
+  }
+  boxesContainer.append(...boxes);
+}
+
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
